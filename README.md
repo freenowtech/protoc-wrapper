26b620c6b1da56c9522a1120058e2d75ca28e8a6
Lucas Laurindo dos Santos <lucas.ls@mytaxi.com>
2019-07-02 14:29:34 +0200

    Adding install and docs

---
+++ README.md
@@ -0,0 +1,29 @@
+# Protoc Wrapper
+
+A simple wrapper for Google Protocol Buffer's "protoc" utility
+
+## Install
+
+```bash
+/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/lucasls/protoc-wrapper/master/install.sh)"
+```
+
+The target version will be asked during installation.
+
+## Usage
+
+```bash
+./protocw [args] # instead of protoc [args]
+```
+
+```bash
+./protocw --configure # So change the target protoc version
+```
+
+```bash
+./protocw --self-update # Updates Protoc Wrapper to the latest version
+```
+
+## Author
+
+Lucas Laurindo dos Santos @ [FREE NOW](https://free-now.com/)
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+INSTALL_BASE_URL="https://raw.githubusercontent.com/lucasls/protoc-wrapper/master"
+
+print() {
+    printf "$1" >&2
+}
+
+println() {
+    echo "$1" >&2
+}
+
+print "Downloading Protoc Wrapper... "
+curl "$INSTALL_BASE_URL/protocw" -o protocw.new --fail --silent
+last_error=$?
+
+if [ $last_error -ne 0 ]; then
+    println "Error"
+    println "Could not download Protoc Wrapper from $INSTALL_BASE_URL"
+    exit $last_error
+fi
+
+println "Done"
+
+println "Moving stuff around"
+if [ -f protocw ]
+then
+    mv -f protocw protocw.old
+fi
+
+mv protocw.new protocw
+
+println "Setting permissions"
+chmod +x protocw
+
+println "Cleaning up"
+rm -f protocw.old
+
+if [ ! -f protocw.env ]
+then
+    ./protocw --configure
+fi
+
+println "Protoc Wrapper successfully installed"
